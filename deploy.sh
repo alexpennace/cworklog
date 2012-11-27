a40f82bb457e839a68d2e194e21a0e43a1c9b959
Jim A Kinsman <jakinsman@gmail.com>
2012-11-27 00:21:22 -0600

    changed deploy script a bit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DATE="`date +%Y-%m-%d_%k%M`"
+DATE="`date +%Y-%m-%d_%H%M`"
 TARFILE="releases/cwl_mods$DATE.tar.bz2"
 DIFFCMD="git diff --name-only HEAD^! --diff-filter=MA"
 TARCMD="tar -cjf $TARFILE $(git diff --name-only HEAD^! --diff-filter=MA)"
