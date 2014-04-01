a15b193eca29b197e20cce84e7a3e607219815cc
Jim A Kinsman <jakinsman@gmail.com>
2014-04-01 07:24:58 -0500

    better logo size and link

--- public_html/lib/Site.class.php
+++ public_html/lib/Site.class.php
@@ -124,7 +124,7 @@
          if (empty($width)){ $w = false; }else{ $w = true; }
          ?>
           <?PHP if (!$w){ $width = '26'; } ?>
-          <img id="ul_logo" style="width: 100px" src="images/Inr_hdr_logo.jpg">
+          <a href="work_log.php" title="All Work Logs"><img id="ul_logo" style="width: 117px" src="images/Inr_hdr_logo.jpg"></a>
 
           <a href="#" title="Refresh Content" onclick="window.location.href = window.location.href; return false"><img border=0 src="images/refresh.png" style="width: <?=$width?>px; margin-top:5px;" align="top"></a>
           <?PHP if (!$w){ $width = '24'; } ?>
