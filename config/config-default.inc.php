437693e11e3831f2f47465b3f1a6cc5dcbb94eb2
Jim A Kinsman <jakinsman@gmail.com>
2014-04-09 23:44:50 -0500

    new config file location

---
+++ config/config-default.inc.php
@@ -0,0 +1,36 @@
+<?php
+      //this file needs to be defined and then renamed to config.inc.php in order for the server to work
+      //create user 'USER'@'localhost' IDENTIFIED BY 'PASSWORD';
+      $cfg['db_host'] = 'localhost';
+      $cfg['db_user'] = 'dbuser';
+      $cfg['db_pass'] = 'dbpass';
+      $cfg['db']      = 'work_log';
+      //$cfg['db_dsn'] = '';   //optionally define full PDO DSN
+
+      $cfg['site_title'] = "Contractor's Work Log";
+      
+      $cfg['use_php_mail'] = false;
+      $cfg['email_from_header'] = 'From: Contractor\'s Work Log <noreply@cworklog.com>';
+      $cfg['insert_mock_company_upon_registration'] = true;
+      
+      //$cfg['stripe_apikey'] = "<PUT-YOUR-APIKEY-HERE>";
+
+      $cfg['environment'] = 'development'; ///or production 
+
+      $cfg['verbose_debugging'] = false;   //verbose debugging will show SQL statements, etc
+      
+      $cfg['domain'] = 'cworklog.com';
+
+      $cfg['default_from_email'] = 'noreply@'.$cfg['domain'];
+      $cfg['base_url'] = 'http://cworklog.com';
+
+      /* TO ENABLE EMAIL USING SMTP UNCOMMENT BELOW AND FILL
+      $cfg['smtp'] = array(
+            'server'=>'smtp.gmail.com', 
+            'port'=> 465, 
+            'protocol'=>'ssl',
+            'username'=> '', 
+            'password'=> '', 
+      );
+      */
+
