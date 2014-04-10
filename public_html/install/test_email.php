056a5b61cd935d2b141c66ab1d68b3fe175883db
Jim A Kinsman <jakinsman@gmail.com>
2014-04-09 21:13:13 -0500

    quick-commit-Wed Apr  9 21:13:13 CDT 2014

---
+++ public_html/install/test_email.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(__DIR__.'/../lib/cwl_email.class.php');
+
+list($mailer, $message, $logger) = cwl_email::setup(true);
+
+$message->setSubject('Contractor\'s Work Log - Test Email');
+$message->setBody("This is a <b>test</b> email", 'text/html');
+
+ 
+$message->setTo(array($_GET['email']));
+$message->setBcc(array('cworklog@gmail.com'));
+
+$result = $mailer->send($message);
+
+if ($mailer->send($message, $failures)){
+		echo 'Sent!';
+}else{
+		echo 'Failed to send!'.$failures;
+}
